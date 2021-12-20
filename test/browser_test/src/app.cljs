17d222109247d09fc7ed683ebba6fc05cb49e273
Doug Hamilton <doug.hamil@gmail.com>
2021-12-20 14:13:57 -0700

    karma

--- test/browser_test/src/app.cljs
+++ test/browser_test/src/app.cljs
@@ -1,7 +1,12 @@
 (ns app
-  (:require [jx.reporter.karma :refer-macros [run-all-tests]]))
+  (:require [jx.reporter.karma :refer-macros [run-all-tests]]
+            [cljs.test :refer-macros [deftest is testing]]))
 
 (enable-console-print!)
+
+(deftest noop-test
+  (testing "NOOP test"
+    (is (= 1 1))))
 
 (defn ^:export run-all [karma]
   (run-all-tests karma))
