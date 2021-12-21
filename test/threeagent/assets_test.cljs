6146229f7168cce6f5a02d02b9d67b52e1148eeb
Doug Hamilton <doug.hamil@gmail.com>
2021-12-20 18:48:40 -0700

    add texture loader test

--- test/threeagent/assets_test.cljs
+++ test/threeagent/assets_test.cljs
@@ -1,23 +1,23 @@
 (ns threeagent.assets-test
   (:require [threeagent.assets :as sut]
             [cljs.test :refer [async]
-                       :refer-macros [deftest is testing]]))
+                       :refer-macros [deftest is]]))
 
+(def ^:private asset-tree
+  [["/assets"
+    ["/models" {:loader sut/model-loader}
+     ["alien.glb" :model/alien {}]]
+    ["/textures" {:loader sut/texture-loader}
+     ["black.png" :texture/black {}]]]])
 
-(deftest model-load-test
-  (testing "loading model file"
-    (async done
-           (let [db (atom {})]
-             (->
-              (sut/load! db [["/assets"
-                              ["/models" {:loader sut/model-loader}
-                               ["alien.glb" :model/alien {}]]]])
-              (.then (fn []
-                       (is (some? (:model/alien @db)))
-                       (done)))
-              (.catch (fn [err]
-                        (is (nil? err))
-                        (done))))))))
-      
-
-
+(deftest loaders-test
+  (async done
+         (let [db (atom {})]
+           (-> (sut/load! db asset-tree)
+               (.then (fn []
+                        (is (some? (:model/alien @db)))
+                        (is (some? (:texture/black @db)))
+                        (done)))
+               (.catch (fn [error]
+                         (is (nil? error))
+                         (done)))))))
